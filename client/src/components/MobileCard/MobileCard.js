d1b5e15beef4f775c1f0f925b56bf7db40312420
RutikPetkar98 <rutikpetkar431@gmail.com>
2024-04-09 22:54:35 +0530

    small changes

--- client/src/components/MobileCard/MobileCard.js
+++ client/src/components/MobileCard/MobileCard.js
@@ -14,7 +14,7 @@ function MobileCard({_id,url,title,content,price}) {
         <h1 className='mobile-title'>{title}</h1>
         <p className='mobile-content'>{content.substring(0,130)} {title.length > 20 ? "..." : <Link to={`/mobile-blog/${_id}`}>Read More</Link>}</p>
         
-        
+        <h2 className='mobile-price'>₹ {price}</h2>
     
 
         </div>
